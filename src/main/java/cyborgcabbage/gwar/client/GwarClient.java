faeb46633607b67495b83f29eb39ec7461b0efb5
CyborgCabbage <finlaylaidlaw9@gmail.com>
2021-11-02 11:43:55 +0000

    Initial commit

---
+++ src/main/java/cyborgcabbage/gwar/client/GwarClient.java
@@ -0,0 +1,13 @@
+package cyborgcabbage.gwar.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class GwarClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
